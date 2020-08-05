720f359091f2a466aa2b80128bc4789c7d4be4bf
Brett Allen <bzallen@google.com>
2020-08-05 20:32:01 +0000

    added chart library

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,22 +12,37 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
 /**
  * Fetches the data from the DataServlet and displays.
  */
-function getDataServlet() {/* exported getDataServlet */
-  fetch('/data').then((response) => response.json()).then((messages) => {
-    const messagesListElement = document.getElementById('messages-container');
-    messagesListElement.innerHTML = '';
-    for (let i = 0; i < messages.length; i++) {
-      messagesListElement.appendChild(createListElement(messages[i]));
-    }
-  });
-}
+function drawChart() {/* exported drawChart */
+  const data = new google.visualization.DataTable();
+  data.addColumn('string', 'Activity');
+  data.addColumn('number', 'Hours');
+  data.addRows([
+    ['Volleyball', 2],
+    ['Lifting Weights', 1],
+    ['In Meetings', 2],
+    ['Debugging', 5],
+    ['Staring at his computer, helpless', 0.5],
+    ['Re-running the same code hoping it will work that time', 0.5],
+    ['Actually coding new stuff', 1],
+    ['Dinner with the family', 2],
+    ['Trying to decide what to watch', 2],
+    ['Actually watching something', 1],
+    ['Sleeping', 7]
+  ]);
 
-/** Creates an <li> element containing text. */
-function createListElement(text) {
-  const liElement = document.createElement('li');
-  liElement.innerText = text;
-  return liElement;
+  const options = {
+    'title': 'How Brett Spends His Day',
+    'width':600,
+    'height':700
+  };
+
+  const chart = new google.visualization.PieChart(
+      document.getElementById('chart-container'));
+  chart.draw(data, options);
 }
